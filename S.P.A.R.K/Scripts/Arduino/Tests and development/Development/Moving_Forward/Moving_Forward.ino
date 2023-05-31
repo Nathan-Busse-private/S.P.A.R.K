b858e60baad9177fd959ee15d3c132bd4d2757a9
Nathan Busse <82398683+Nathan-Busse@users.noreply.github.com>
2023-05-31 16:49:22 +0200

    update
    S.P.A.R.K begins to walk

---
+++ S.P.A.R.K/Scripts/Arduino/Tests and development/Development/Moving_Forward/Moving_Forward.ino
@@ -0,0 +1,155 @@
+/* S.P.A.R.K moving forward
+   Project: S.P.A.R.K
+   Start date: 31 May 2023
+   Last edited: 31 May 2023
+   Info:
+
+  My first attempt to make S.P.A.R.K walk forward for the first time.
+
+  My walking theory:
+  ------------------
+  Their will 2 legs touching the ground and 2 in the air fpr each step.
+  The 2 legs will operate in sync with the other leg diagnal from its partner.
+  For example:
+
+  Leg A will partner with Leg C
+  Leg B will partner with Leg D.
+
+  The Tibia of the paired legs will lift first and the Femur will then lift second. 
+  Once the second pair of legs begin to lift, the already raised Femur will then lower
+  first then the Tibia will lower second which follows on indefinitly, thus making the robot walk.
+
+  Made by Nathan-Busse.
+*/
+
+// Calling Libraries
+
+#include <Adafruit_PWMServoDriver.h>
+#include <Wire.h>
+
+// Declaring pins
+
+// Pin declarations
+#define Coxae_A 0
+#define Femur_A 1
+#define Tibia_A 2
+
+#define Coxae_B 4
+#define Femur_B 5
+#define Tibia_B 6
+
+#define Coxae_C 8
+#define Femur_C 9
+#define Tibia_C 10
+
+#define Coxae_D 12
+#define Femur_D 13
+#define Tibia_D 14
+
+// Leg position identification
+
+// Standing position
+#define C_idle 90       // Coxae idle
+#define FF_stand_L 135  // Femur left while standing
+#define FF_stand_R 45   // Femur right while standing
+
+#define BF_stand_L 135  // Femur left while standing
+#define BF_stand_R 45   // Femur right while standing
+
+#define FT_stand 50  // Tibia stand
+#define BT_stand 65
+
+// Define time delay
+//#define wait 1000   // Period of time to pause the servos before transitioning to the standing position.
+#define hold 200  // Period of time to remain standing.
+#define pause 200
+// Walking position
+
+// Tibia
+#define FT_Rise 30
+#define BT_Rise 40
+#define FT_Lower 50
+#define BT_Lower 60
+
+// Femur
+#define FF_Rise_L 125
+#define BF_Rise_L 125
+#define FF_Lower_L 135
+#define BF_Lower_L 135
+
+#define FF_Rise_R 35
+#define BF_Rise_R 35
+#define FF_Lower_R 45
+#define BF_Lower_R 45
+
+// PWM setup
+#define MIN_PulseLength 500
+#define MAX_PulseLength 2500
+#define MED_PulseLength 2500
+#define Frequency 50  // 50 HRz
+
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+int degrees;
+
+void setup() {
+  pwm.begin();
+  pwm.setPWMFreq(Frequency);
+}
+
+void loop() {
+  // Stand position
+  pwm.setPWM(Coxae_A, 0, pulseWidth(C_idle));
+  pwm.setPWM(Coxae_B, 0, pulseWidth(C_idle));
+  pwm.setPWM(Coxae_C, 0, pulseWidth(C_idle));
+  pwm.setPWM(Coxae_D, 0, pulseWidth(C_idle));
+
+  pwm.setPWM(Femur_A, 0, pulseWidth(FF_stand_L));
+  pwm.setPWM(Femur_B, 0, pulseWidth(BF_stand_L));
+  pwm.setPWM(Femur_C, 0, pulseWidth(BF_stand_R));
+  pwm.setPWM(Femur_D, 0, pulseWidth(FF_stand_R));
+
+  pwm.setPWM(Tibia_A, 0, pulseWidth(FT_stand));
+  pwm.setPWM(Tibia_B, 0, pulseWidth(BT_stand));
+  pwm.setPWM(Tibia_C, 0, pulseWidth(BT_stand));
+  pwm.setPWM(Tibia_D, 0, pulseWidth(FT_stand));
+  delay(hold);
+
+  // Walking position
+  pwm.setPWM(Coxae_A, 0, pulseWidth(C_idle));
+  pwm.setPWM(Coxae_C, 0, pulseWidth(C_idle));
+  pwm.setPWM(Coxae_B, 0, pulseWidth(C_idle));
+  pwm.setPWM(Coxae_D, 0, pulseWidth(C_idle));
+
+  // Rise
+  pwm.setPWM(Tibia_A, 0, pulseWidth(FT_Rise));
+  pwm.setPWM(Tibia_C, 0, pulseWidth(BT_Rise));
+  pwm.setPWM(Femur_A, 0, pulseWidth(FF_Rise_L));
+  pwm.setPWM(Femur_C, 0, pulseWidth(BF_Rise_R));
+  delay(pause);
+
+  pwm.setPWM(Tibia_D, 0, pulseWidth(FT_Rise));
+  pwm.setPWM(Tibia_B, 0, pulseWidth(BT_Rise));
+  pwm.setPWM(Femur_D, 0, pulseWidth(FF_Rise_R));
+  pwm.setPWM(Femur_B, 0, pulseWidth(BF_Rise_L));
+  
+  // Lower
+  pwm.setPWM(Femur_A, 0, pulseWidth(FF_Lower_L));
+  pwm.setPWM(Femur_C, 0, pulseWidth(BF_Lower_R));
+  pwm.setPWM(Tibia_A, 0, pulseWidth(FT_Lower));
+  pwm.setPWM(Tibia_C, 0, pulseWidth(BT_Lower));
+  delay(pause);
+
+  pwm.setPWM(Femur_D, 0, pulseWidth(FF_Lower_L));
+  pwm.setPWM(Femur_B, 0, pulseWidth(BF_Lower_R));
+  pwm.setPWM(Tibia_D, 0, pulseWidth(FT_Lower));
+  pwm.setPWM(Tibia_B, 0, pulseWidth(BT_Lower));
+}
+
+// Pulsewidth declaration
+int pulseWidth(int angle) {
+  int pulse_wide, analog_value;
+  pulse_wide = map(angle, 0, 180, MIN_PulseLength, MAX_PulseLength);
+  analog_value = int(float(pulse_wide) / 1000000 * Frequency * 4096);
+  Serial.println(analog_value);
+  return analog_value;
+}
