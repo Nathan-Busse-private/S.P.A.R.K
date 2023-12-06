e830af82c94d4f949c26b269ba01a4c9d0abe266
Nathan Busse <82398683+Nathan-Busse@users.noreply.github.com>
2023-12-06 11:24:38 +0200

    Hardware Update
    Redesigned the Femur attachment bracket to utilize the bearing used in the Femur model.

---
+++ S.P.A.R.K/Nightly/Scripts/C++/DS3235 servo/v1/Tests and development/Development/v1/DS3235_Bluetooth_Nightly_v1/DS3235_Bluetooth_Nightly_v1.ino
@@ -0,0 +1,17 @@
+
+
+#include <Wire.h>
+#include <SoftwareSerial.h>
+#include <spark_pwm.h>
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
